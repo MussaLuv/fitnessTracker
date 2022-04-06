47a72c87534db07a11c1c336cae55daeaed039b6
JeongwookNa <najung936@hotmail.com>
2022-04-06 10:32:11 -0500

    started on users

---
+++ db/users.js
@@ -0,0 +1,25 @@
+const client = require('./clinet');
+const bcrypt = require('bcrypt');
+
+// inside of createUser({ username, password})
+async function createUser({})
+const SALT_COUNT = 10;
+
+bcrypt.hash(password, SALT_COUNT, function(err, hashedPassword) {
+  createUser({
+    username,
+    password: hashedPassword // not the plaintext
+  });
+});
+
+// inside of getUser({username, password})
+const user = await getUserByUserName(username);
+const hashedPassword = user.password;
+
+bcrypt.compare(password, hashedPassword, function(err, passwordsMatch) {
+  if (passwordsMatch) {
+    // return the user object (without the password)
+  } else {
+    throw SomeError;
+  }
+});
