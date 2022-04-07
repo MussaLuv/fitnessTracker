f6d1125e62cf2892a07ae6b07847ec39fbe72801
JeongwookNa <najung936@hotmail.com>
2022-04-07 10:45:04 -0500

    activities.js adjusted from branch -CW

--- db/activities.js
+++ db/activities.js
@@ -1,24 +1,41 @@
+const client = require('./client');
 
-async function getAllActivities() {
-    try {
-        const { rows } = await client.query(`
-        SELECT *
-        FROM activities;
-        `);
+async function attachActivitiesToRoutines(routines) {
+  // no side effects
+  const routinesToReturn = [...routines];
+  const binds = routines.map((_, index) => `$${index + 1}`).join(', ');
+  const routineIds = routines.map(routine => routine.id);
+  if (!routineIds?.length) return [];
+  
+  try {
+    // get the activities, JOIN with routine_activities (so we can get a routineId), and only those that have those routine ids on the routine_activities join
+    const { rows: activities } = await client.query(`
+      SELECT activities.*, routine_activities.duration, routine_activities.count, routine_activities.id AS "routineActivityId", routine_activities."routineId"
+      FROM activities 
+      JOIN routine_activities ON routine_activities."activityId" = activities.id
+      WHERE routine_activities."routineId" IN (${ binds });
+    `, routineIds);
 
-        return rows;
-    } catch (error) {
-        throw error;
+    // loop over the routines
+    for(const routine of routinesToReturn) {
+      // filter the activities to only include those that have this routineId
+      const activitiesToAdd = activities.filter(activity => activity.routineId === routine.id);
+      // attach the activities to each single routine
+      routine.activities = activitiesToAdd;
     }
+    return routinesToReturn;
+  } catch (error) {
+    throw error;
+  }
 }
 
-async function createActivity({ name, description }) {
+//fetches activity IDs
+async function getActivityById(id) {
     try {
-        const { rows: [ activity ] } = await client.query(`
-        INSERT INTO activities (name, description) 
-        VALUES($1, $2) 
-        RETURNING *;
-      `, [name, description]);
+        const { rows: [activity] } = await client.query(`
+        SELECT * FROM activities
+        WHERE id = $1
+        `, [id])
 
         return activity;
     } catch (error) {
@@ -26,38 +43,55 @@ async function createActivity({ name, description }) {
     }
 }
 
-async function attachActivitiesToRoutines(routines) {
-    // no side effects
-    const routinesToReturn = [...routines];
-    const binds = routines.map((_, index) => `$${index + 1}`).join(', ');
-    const routineIds = routines.map(routine => routine.id);
-    if (!routineIds?.length) return [];
-    
+//grabs all activities
+async function getAllActivities() {
     try {
-      // get the activities, JOIN with routine_activities (so we can get a routineId), and only those that have those routine ids on the routine_activities join
-      const { rows: activities } = await client.query(`
-        SELECT activities.*, routine_activities.duration, routine_activities.count, routine_activities.id AS "routineActivityId", routine_activities."routineId"
-        FROM activities 
-        JOIN routine_activities ON routine_activities."activityId" = activities.id
-        WHERE routine_activities."routineId" IN (${ binds });
-      `, routineIds);
-  
-      // loop over the routines
-      for(const routine of routinesToReturn) {
-        // filter the activities to only include those that have this routineId
-        const activitiesToAdd = activities.filter(activity => activity.routineId === routine.id);
-        // attach the activities to each single routine
-        routine.activities = activitiesToAdd;
-      }
-      return routinesToReturn;
-    } catch (error) {
-      throw error;
+        const { rows } = await client.query(`
+        SELECT * FROM activities
+        `);
+
+        return rows;
+    } catch(error) {
+        throw error;
     }
-  }
+}
 
+//creates new activities
+async function createActivity({ name, description }) {
+    try {
+        const { rows: [activity] } = await client.query(`
+            INSERT INTO activities(name, description)
+            VALUES ($1, $2)
+            ON CONFLICT (name) DO NOTHING
+            RETURNING *;
+            `, [name, description]
+        );
+        return activity;
+} catch (error) {
+    throw error;
+    }
+}
 
-  module.exports = {
+//updates names and descriptions of the activities without changing any IDs
+async function updateActivity({ id, name, description }) {
+    try {
+        const { rows: [activity] } = await client.query(`
+        UPDATE activities 
+        SET name = $2, description = $3
+        WHERE id = $1
+        RETURNING *;
+        `, [id, name, description])
+
+        return activity
+    } catch (error) {
+        throw error;
+    }
+}
+
+module.exports = {
+    attachActivitiesToRoutines,
+    getActivityById,
     getAllActivities,
     createActivity,
-    attachActivitiesToRoutines,
-}
+    updateActivity
+} 
