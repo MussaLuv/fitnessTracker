14a3d9a34e589642bcaf6753a16b9301cd24b668
JeongwookNa <najung936@hotmail.com>
2022-04-07 10:43:25 -0500

    users.js

--- db/users.js
+++ db/users.js
@@ -1,34 +1,74 @@
 const client = require('./clinet');
-const bcrypt = require('bcrypt');
 
 // inside of createUser({ username, password})
-async function createUser({}){
-    const SALT_COUNT = 10;
+async function createUser({ username, password }){
 
-    bcrypt.hash(password, SALT_COUNT, function(err, hashedPassword) {
-    createUser({
-        username,
-        password: hashedPassword // not the plaintext
-  });
-})
-};
+    try{
+        const {rows: [user] } = await client.query(`
+        INSERT INTO users(username, password)
+        VALUES ($1, $2)
+        RETURNING *;`
+        , [username, password]);
+
+        delete user.password;        
+        return user;
+    }catch (error){
+        throw error;
+    }
+}
 
 // inside of getUser({username, password})
-async function getUser({ username, password}) {
-const user = await getUserByUserName(username);
-const hashedPassword = user.password;
-
-bcrypt.compare(password, hashedPassword, function(err, passwordsMatch) {
-  if (passwordsMatch) {
-    // return the user object (without the password)
-  } else {
-    throw SomeError;
+async function getUser({username, password}){
+  try {
+      const {rows: [user], } = await client.query(`
+      SELECT * 
+      FROM users
+      WHERE username = $1;
+      `, [username]);
+  if (user.password !== password){
+      return
   }
-})
-};
+      
+      delete user.password;
+      return user;
+  }catch (error){
+      throw error;
+  }
+}
+
+async function getUserById(id){
+  try{
+      const {rows: [user], } = await client.query(`
+      SELECT * 
+      FROM users
+      WHERE id = $1;
+      `, [id]);
+
+      return user;
+  }catch (error){
+      throw error;
+  }
+}
+
+
+async function getUserByUsername(username){
+    
+  try {
+      const {rows: [user], } = await client.query(`
+      SELECT id, username 
+      FROM users
+      WHERE username = $1;
+      `, [username]);
+
+      return user;
+  }catch (error){
+      throw error;
+  }
+}
 
 module.exports = {
     createUser,
     getUser,
+    getUserById,
     getUserByUsername
-}
+};
