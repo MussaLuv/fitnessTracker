23becd2ae22ea343df318f08c68ae5670915a075
Aaron Reyes <ProdAronBeatz@gmail.com>
2022-04-07 10:01:49 -0500

    Pushing again to see my work in db

---
+++ db/activities.js
@@ -0,0 +1,57 @@
+const client = require("./client");
+
+async function createActivity({ name, description }) {
+  const {
+    rows: [activity],
+  } = await client.query(
+    `
+        INSERT INTO activities (name, description)
+        VALUES ($1, $2)
+        RETURNING *;
+        `,
+    [name, description]
+  );
+  return activity;
+}
+
+async function getAllActivities() {
+  const { rows } = await client.query(
+    `
+        SELECT * FROM activities;`
+  );
+  return rows;
+}
+
+async function updateActivity({ id, name, description }) {
+  const {
+    rows: [activities],
+  } = await client.query(
+    `
+        UPDATE activities
+        SET name = $2, description = $3
+        WHERE id = $1
+        RETURNING *;
+        `,
+    [id, name, description]
+  );
+  return activities;
+}
+
+async function getActivityById(id) {
+  const {
+    rows: [activity],
+  } = await client.query(
+    `
+        SELECT * FROM activities WHERE id = $1
+        `,
+    [id]
+  );
+  return activity;
+}
+
+module.exports = {
+  createActivity,
+  getAllActivities,
+  updateActivity,
+  getActivityById,
+};
--- db/index.js
+++ db/index.js
@@ -1,1 +1,38 @@
 // require and re-export all files in this db directory (users, activities...)
+const { createUser, getUser, getUserById } = require("./users");
+const {
+  createActivity,
+  getAllActivities,
+  updateActivity,
+  getActivityById,
+} = require("./activities");
+const { addActivityToRoutine } = require("./routine_activities");
+const {
+  createRoutine,
+  getRoutineById,
+  getRoutinesWithoutActivities,
+  getAllRoutines,
+  getAllPublicRoutines,
+  getAllRoutinesByUser,
+  getPublicRoutinesByUser,
+  getPublicRoutinesByActivity,
+} = require("./routines");
+
+module.exports = {
+  createUser,
+  createActivity,
+  createRoutine,
+  getRoutineById,
+  getRoutinesWithoutActivities,
+  getAllActivities,
+  addActivityToRoutine,
+  getUser,
+  getUserById,
+  updateActivity,
+  getActivityById,
+  getAllRoutines,
+  getAllPublicRoutines,
+  getAllRoutinesByUser,
+  getPublicRoutinesByUser,
+  getPublicRoutinesByActivity,
+};
