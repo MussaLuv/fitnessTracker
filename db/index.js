5a02f5f86822ac11d71412140534d5365664c919
Aaron Reyes <ProdAronBeatz@gmail.com>
2022-04-17 19:48:44 -0500

    Added the final index client

--- db/index.js
+++ db/index.js
@@ -1,7 +1,15 @@
+const client = new Client({
+  connectionString:
+    process.env.DATABASE_URL || "postgres://localhost:5432/juicebox-dev",
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : undefined,
+});
 // require and re-export all files in this db directory (users, activities...)
 module.exports = {
-    ...require('./users'), // adds key/values from users.js
-    ...require('./activities'), // adds key/values from activites.js
-    ...require('./routines'), // etc
-    ...require('./routine_activities') // etc
-  }
+  ...require("./users"), // adds key/values from users.js
+  ...require("./activities"), // adds key/values from activites.js
+  ...require("./routines"), // etc
+  ...require("./routine_activities"), // etc
+};
